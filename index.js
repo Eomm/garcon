cdbf190b48b0c30ff7ef94caa30cee53f3f3247d
Manuel Spigolon <behemoth89@gmail.com>
2025-01-01 08:59:03 +0100

    feat: download tdg

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const assert = require('node:assert')
+const { action: downloadTDG } = require('./actions/download-tdg')
+
+async function run (args, env) {
+
+  const action = args[0]
+
+  switch (action) {
+    case 'download-tdg':
+      assert.ok(env.TDG_USER, 'TDG_USER is required')
+      assert.ok(env.TDG_PASSWORD, 'TDG_PASSWORD is required')
+      await downloadTDG(args, env)
+      break
+    default:
+      throw new Error(`Action ${action} not found`)
+  }
+
+}
+
+run(process.argv.slice(2), process.env)
+  .then(() => console.log('Done'))
+  .catch(error => console.error('Error:', error))
+
